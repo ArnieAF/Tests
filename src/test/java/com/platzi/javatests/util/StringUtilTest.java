3cc46c0a03e1541535259556cad7b1172b873db4
Arnie Adriano <arnie.7u7@gmail.com>
2024-10-29 20:16:02 +0100

    Test

--- src/test/java/com/platzi/javatests/util/StringUtilTest.java
+++ src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -1,17 +1,38 @@
 package com.platzi.javatests.util;
 
 
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+
 public class StringUtilTest {
 
-    public static void main(String[] args) {
+    @Test
+    public void repeat_string_once(){
 
-       String result =  StringUtil.repeat("hola",3);
-        if(result.equals("holaholahola"))
-            System.out.println("OK");
+        Assertions.assertEquals("hola",StringUtil.repeat("hola",1));
 
-        String result2 =  StringUtil.repeat("hola",1);
-        if(result2.equals("hola"))
-            System.out.println("OK");
     }
+    @Test
+    public void repeat_string_multiple_times(){
+
+        Assertions.assertEquals("holaholahola",StringUtil.repeat("hola",3));
+
+    }
+    @Test
+    public void repeat_string_zerp_times(){
+
+        Assertions.assertEquals("",StringUtil.repeat("hola",0));
+
+    }
+    @Test
+    public void repeat_string_negative_times(){
+
+        Assertions.assertThrows(IllegalArgumentException.class,()->StringUtil.repeat("Hola",-3));
+
+    }
+
 
 }
